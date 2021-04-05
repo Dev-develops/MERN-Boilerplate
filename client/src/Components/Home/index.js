df2398aa6655facc4c04188e2db1828ab442711c
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-05 13:25:17 +0530

    Styled Components

---
+++ client/src/Components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    Section
+} from './HomeComponents.js';
+
+function Home() {
+
+    return (
+        <Section>
+        </Section>
+    )
+}
+
+export default Home;
