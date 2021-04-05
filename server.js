0326bb9080861eca4d6bdb5d036fab2bbe5ca7ed
Devendra Pratap Singh <ashudev3254@gmail.com>
2021-04-05 13:50:24 +0530

    Backend Complete

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//import the routes that you want to use for specific routes.
+
 app.use(express.json())
 app.use(cors({
     origin: "http://localhost:3000"
